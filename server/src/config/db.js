cd08147e720ef44adb366663fb18253878ee0eb8
makandarSahil <sahilmakandar15@gmail.com>
2025-01-07 16:18:39 +0530

    postgresql connection and prisma db

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,12 +1,17 @@
-import mongoose from "mongoose";
+import pkg from 'pg';
+const { Client } = pkg;
 
 const ConnectDB = async () => {
+  const url = process.env.DATABASE_URL;
+
+  const client = new Client({ connectionString: url });
+
   try {
-    const url = process.env.MONGOD_URI;
-    await mongoose.connect(url);
-    console.log("Mongodb Connected......");
-  } catch (error) {
-    console.log("Error: ", error);
+    await client.connect();
+    console.log("Connected to the database");
+    return client;
+  } catch (err) {
+    console.error("Error connecting to the database:", err.message);
     process.exit(1);
   }
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import ConnectDB from "./config/db.js";
+import cors from "cors";
+import GlobalErrorHandler from "./config/errorHandler.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
 // middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+  })
+);
+app.use(express.json());
 
+app.use(GlobalErrorHandler);
+  
 
-
-ConnectDB()
+function serverStart() {
+  ConnectDB()
   .then(() => {
     app.on("error", (err) => {
       throw err;
@@ -22,3 +32,6 @@ ConnectDB()
   .catch((err) => {
     console.log("Error: ", err);
   });
+}  
+
+serverStart()
